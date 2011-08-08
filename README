c3495d21094c605503495bf2991e6e818592af21
Sean Goller <sean@goller.net>
2011-08-07 19:11:46 -0700

    Initial version.

---
+++ README
@@ -0,0 +1,2 @@
+RangeHTTPServer is a lightweight Python module that extends SimpleHTTPServer
+to support the HTTP Range command.
---
+++ setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+setup(name='RangeHTTPServer',
+      version='0.1',
+      py_modules=['RangeHTTPServer'],
+      )
